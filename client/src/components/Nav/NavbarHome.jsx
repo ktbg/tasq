43eb94a58b9745843ab361b4032543fd5e7cc397
ktbg <ktboates@me.com>
2021-09-25 19:46:59 -0400

    accessibility adds for text in nav links

--- client/src/components/Nav/NavbarHome.jsx
+++ client/src/components/Nav/NavbarHome.jsx
@@ -8,12 +8,12 @@ import BrandLogo from './BrandLogo';
 export default function Navbar() {
   return (
     <div className="flex justify-between px-6 mt-11 h-11">
-      <div className="object-left py-4">
+      <div className="object-left py-4" alt="link to home page">
         <NavLink to="/">
           <BrandLogo />
         </NavLink>
       </div>
-      <div className="float-right inline-block p-2">
+      <div className="float-right inline-block p-2" alt="create new list">
         <NavLink to="/new">
           <Media queries={{ iPad:"(min-width: 1024px)"}}>
               {matches =>
