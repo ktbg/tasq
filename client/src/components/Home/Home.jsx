ecd65887b66d7683f4c865902de07f554ea745c5
ktbg <ktboates@me.com>
2021-09-26 16:30:04 -0400

    adjusted margin on desktop layout

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -26,17 +26,17 @@ export default function Home() {
 
   return (
     <div>
-      <div className="flex flex-col w-10/12 sm:w-1/2 md:w-3/5 mt-8 mx-auto ">
+      <div className="flex flex-col w-10/12 sm:w-1/2 md:w-4/6 mt-8 mx-auto ">
         <div className="mt-8 ml-4 md:pl-8 w-375 mx-auto">
           <h1 className="w-full text-left pl-6 font-medium text-2xl">My Lists</h1>
         </div>
         <div className="flex flex-col items-start sm:items-center mx-auto ml-6 mt-8"> 
 
           {/* ------------------------------ lists ---------------------------------- */}
-          <div className="flex flex-col md:flex-row md:flex-wrap md:justify-around">
+          <div className="flex flex-col md:flex-row md:flex-wrap md:justify-around ">
           {lists.map((list)=> (
             <Link to={`/list/${list.id}/${list.fields.name}`} key={list.id}>
-              <div className="bg-tasqGrey hover:bg-tasqHover shadow h-19 w-80 mb-4 rounded px-4 py-4 flex justify-between" key={list.id}>
+              <div className="bg-tasqGrey hover:bg-tasqHover shadow h-19 w-80 mb-4 rounded px-4 py-4 flex justify-between md:mx-2" key={list.id}>
                 <div>
                   <h3 className="font-medium text-left w-66 py-3.75">{list.fields.name}</h3>
                   <TotalListItems id={list.id} className={"text-xs text-left pt-1 font-light"} />
